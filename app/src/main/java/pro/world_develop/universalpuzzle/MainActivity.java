6be7973ce6adf8f7482d85b8da8f93e32c6184d5
Lizica <Lizica1995@gmail.com>
2016-04-22 03:21:23 +0500

    frmStart

--- app/src/main/java/pro/world_develop/universalpuzzle/MainActivity.java
+++ app/src/main/java/pro/world_develop/universalpuzzle/MainActivity.java
@@ -1,13 +1,23 @@
 package pro.world_develop.universalpuzzle;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+
 }
+
