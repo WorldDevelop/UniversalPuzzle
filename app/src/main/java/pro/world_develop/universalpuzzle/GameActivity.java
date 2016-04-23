390a0625c667596aa087b54ead807111c5eafccf
Lizica <Lizica1995@gmail.com>
2016-04-24 01:46:39 +0500

    frmGame

---
+++ app/src/main/java/pro/world_develop/universalpuzzle/GameActivity.java
@@ -0,0 +1,18 @@
+package pro.world_develop.universalpuzzle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+
+
+
+}
